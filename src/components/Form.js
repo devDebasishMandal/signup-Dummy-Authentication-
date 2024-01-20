50860fc8dd81fe6274b8d6aaacdfee3bc290441c
devDebasishMandal <debasishmandal186@gmail.com>
2024-01-20 19:09:04 +0530

    hitting the api request

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Form = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [user, setUser] = useState({});
   function emailInp(e) {
     setEmail(e.target.value);
   }
@@ -14,17 +14,32 @@ const Form = () => {
 
   function handelForm(e) {
     e.preventDefault();
-    console.log(email,password);
+    if (!email || !password) {
+      alert("Please fill all the fields");
+    } else {
+      fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: email,
+          password: password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => setUser(data))
+        .catch((err) => console.log(err));
+    }
+    console.log(email, password);
   }
 
   return (
     <div className="content">
-    <div className="main">
+      <div className="main">
         <p className="welcome">Welcom Back!👋</p>
         <h2>Sign in to your account</h2>
         <form onSubmit={handelForm}>
-        <p>Your email</p>
-          <input type="email" onChange={emailInp} />
+          <p>Your email</p>
+          <input type="text" onChange={emailInp} />
           <p>password</p>
           <input type="password" onChange={passwordInp} />
           <br />
@@ -32,7 +47,7 @@ const Form = () => {
         </form>
         <a href="/">Forgot your password?</a>
       </div>
-     
+
       <p className="foot">
         Don’t have an account?<a href="/">Sign up</a>
       </p>
